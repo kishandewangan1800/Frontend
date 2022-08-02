d478092b673669eff4749504835f3f80a7bb51f4
Kishan Dewangan <kishan.dewangan1800@gmail.com>
2022-08-02 16:49:30 +0530

    Project

--- React/dashboard/src/components/Chat.jsx
+++ React/dashboard/src/components/Chat.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='fixed top-12 bg-white w-72 rounded-xl h-96 m-2 p-2 shadow-xl'
+    style={{zIndex:100}}
+    >
+    Chat Friend
+  </div>
   )
 }
 
--- React/dashboard/src/components/UserProfile.jsx
+++ React/dashboard/src/components/UserProfile.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <div className='fixed top-12 bg-white w-72 rounded-xl h-96 m-2 p-2 shadow-xl'
+    style={{zIndex:100}}
+    >
+    Profile
+  </div>
   )
 }
 
